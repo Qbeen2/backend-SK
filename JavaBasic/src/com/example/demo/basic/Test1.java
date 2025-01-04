d9d9bed258e1e496fa1fa07dd4b22656ed235ded
Qbeen2 <thdrbqls521@gmail.com>
2025-01-04 21:15:00 +0900

    Feat: java 패키지, 구조변경

---
+++ JavaBasic/src/com/example/demo/basic/Test1.java
@@ -0,0 +1,18 @@
+package com.example.demo.basic;
+
+/**
+ *
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        byte i1 =1;
+        short i2 =12;
+        int i3 =1;
+        long i4 =System.currentTimeMillis();
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+    }
+}
